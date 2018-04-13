3b836b09b28211e453a98945b9b3b16dd0b15620
EnaraEtxaniz <enara96etxaniz@gmail.com>
2018-04-13 09:34:54 +0200

    [ADD]UserDAO provisional, no accede a la bd

--- GoogleService/src/main/java/dao/UserDAO.java
+++ GoogleService/src/main/java/dao/UserDAO.java
@@ -1,5 +1,31 @@
 package dao;
 
-public class UserDAO {
+import java.util.Arrays;
+
+import data.User;
+
+public class UserDAO implements IUserDAO{
+	String[] tmpUsers = {
+			"a",
+			"enara.etxaniz@opendeusto.es",
+			"jesus.delapisa@opendeusto.es",
+			"cristian.perez@opendeusto.es",
+			"arosa001@opendeusto.es"};
+
+	String[] tmpPasswords = { 
+			"123",
+			"1234",
+			"12345",
+			"123456",
+			"1234567"
+			};
+	
+	public User getUser(String email) {
+		int index = Arrays.asList(tmpUsers).indexOf(email);
+		String password = tmpPasswords[index];
+		
+		User user = new User(email, password);
+		return user;
+	}
 	
 }
