b69ad02f550f626727ff97ec1b181042a9539151
arosa001 <arosa001@opendeusto.es>
2018-04-13 13:04:05 +0200

    Ventana completa a falta de añadir la base de datos

--- NeverEmptyClient/src/main/java/GUI/VentanaPrincipal.java
+++ NeverEmptyClient/src/main/java/GUI/VentanaPrincipal.java
@@ -19,10 +19,9 @@ import javax.swing.JTextField;
 public class VentanaPrincipal extends JFrame implements ActionListener{
 
 	JLabel lblTitulo;
-	JButton btnPanel1, btnPanel2, btnPanelInterno2;
-	JTextArea textArea;
+	JButton btnPanel1, btnPanel2, btnPanelInterno2,btnSales, btnFavorites, btnRecipes;
+	JTextArea textArea, campo;
 	JPanel panel1, panel2;
-	JTextArea campo;
 	String texto;
 	
 	Vector vProductos;
@@ -31,7 +30,7 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		this.setSize(400, 400);
+		this.setSize(400, 375);
 		this.setTitle("Bienvenido a NeverEmpty!"); 
 		this.setResizable(false);
 		setLayout(null);
@@ -55,7 +54,7 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 		panel2.setVisible(false);
 		
 		lblTitulo = new JLabel ("Seleccione los productos que desea comprar");
-		lblTitulo.setBounds(10,14,221,51);
+		lblTitulo.setBounds(10,14,275,51);
 		
 		btnPanel1 = new JButton("Catalogo");
 		btnPanel1.setBounds(10, 76, 95, 23);
@@ -64,6 +63,15 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 		btnPanel2 = new JButton("Lista de Compra");
 		btnPanel2.setBounds(109, 76, 135, 23);
 		btnPanel2.addActionListener(this);
+		
+		btnSales = new JButton("Ofertas");
+		btnSales.setBounds(10, 315, 95, 23);
+		
+		btnFavorites = new JButton("Favoritos");
+		btnFavorites.setBounds(109, 315, 95, 23);
+		
+		btnRecipes = new JButton("Recetas");
+		btnRecipes.setBounds(208, 315, 95, 23);
 		
 		textArea = new JTextArea();
 		textArea.setBounds(10,117,350,181);
@@ -75,45 +83,49 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 		add(lblTitulo);
 		add(btnPanel1);
 		add(btnPanel2);
+		add(btnSales);
+		add(btnFavorites);
+		add(btnRecipes);
 		
 		add(panel2);
 		add(panel1);
 		
-		
-		//Solucionar boton interno
-
-
 	}
 	
-private void cargarComponentesPanel2() {
+	private void cargarComponentesPanel2() {
 		/*
 		 * listaCompra = new String();
 		 * lblListaCompra = new JLabel;
 		 * */
 		campo = new JTextArea();
 		textArea.setBounds(10, 10, 180, 25);
-		String campo = "Miembros:" + "\nAlvaro" + "\nEnara" + "\nCristian" + "\nJesus" + "\nRebeca";
+		//		String campo = "\nAlvaro";
+		//		textArea.setText(campo);
 	}
 
-private void cargarComponentesPanel1() {
-	textArea = new JTextArea();
-	textArea.setBounds(10, 10, 350, 181);
-//	String texto = "Miembros:" + "\nAlvaro" + "\nEnara" + "\nCristian" + "\nJesus";
-//	textArea.setText(texto);
-	panel1.add(textArea);
-	
-	btnPanelInterno2 = new JButton("Comprar");
-	btnPanelInterno2.setBounds(200, 10, 90, 25);
-	panel1.add(btnPanelInterno2);
-	btnPanelInterno2.addActionListener(this);
-	
+	private void cargarComponentesPanel1() {
+		textArea = new JTextArea();
+		textArea.setBounds(10, 10, 350, 181);
+		String texto = "Productos:";
+		textArea.setText(texto);
+		panel1.add(textArea);
+
+		btnPanelInterno2 = new JButton("Comprar");
+		btnPanelInterno2.setBounds(200, 10, 90, 25);
+		panel1.add(btnPanelInterno2);
+		btnPanelInterno2.addActionListener(this);
+		
+		btnSales.addActionListener(this);
+		btnFavorites.addActionListener(this);
+		btnRecipes.addActionListener(this);
+		
+
 	}
 
 
 
 //ESTO AUN NO SE ESTA UTILIZANDO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //vProductos = new Vector();
-//
 //vProductos = new Vector();
 //vProductos.add("Filetes de pollo");
 //vProductos.add("Dorada");
@@ -130,44 +142,18 @@ private void cargarComponentesPanel1() {
 //vProductos.add("Viangre");
 //vProductos.add("Platanos");
 //vProductos.add("Manzana");
-
 //ESTO AUN NO SE ESTA UTILIZANDO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 //bLogout = new JButton();
 //bRecipes = new JButton();
 //bSales = new JButton();
 //bFavorites = new JButton();
 //bShoppingList = new JButton();
-
-
-
-
-
-//	Botones 
-//	
 //	private JButton bProfile;
 //	private JButton bLogout;
 //	private JButton bRecipes;
 //	private JButton bSales;
 //	private JButton bFavorites;
 //	private JButton bShoppingList;
-
-//	JScrollPane scroll;
-//	JList lProductos;
-//	JTextField producto;
-	
-	
-//	private void configurarVentana() {
-//        this.setBounds((ancho / 2) - (this.getWidth() / 2), (alto / 2) - (this.getHeight() / 2), 500, 500);                                   
-//		this.setResizable(false);
-//	}
-//
-//	
-
-	//Aqui crearemos los dos scrolls
-
-
-
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -176,23 +162,46 @@ private void cargarComponentesPanel1() {
 		v.setVisible(true);
 	}
 
-public void actionPerformed(ActionEvent e) {
-	if (e.getSource()==btnPanel1) {
-		panel1.setVisible(true);
-		panel2.setVisible(false);
-	}
-	
-	if (e.getSource()==btnPanel2) {
-		panel1.setVisible(false);
-		panel2.setVisible(true);
-	}
-	
-	if (e.getSource()==btnPanelInterno2) {
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==btnPanel1) {
+			panel1.setVisible(true);
+			panel2.setVisible(false);
+		}
+
+		if (e.getSource()==btnPanel2) {
+			panel1.setVisible(false);
+			panel2.setVisible(true);
+		}
+
+		if (e.getSource()==btnPanelInterno2) {
+
+
+		}
 		
+		if (e.getSource()==btnSales) {
+
+			VentanaOfertas o = new VentanaOfertas();
+			o.setVisible(true);
+			dispose();
+			
+
+		}
 		
+		if (e.getSource()==btnFavorites) {
+
+			VentanaFavoritos f = new VentanaFavoritos();
+			f.setVisible(true);
+			
+		}
+		
+		if (e.getSource()==btnRecipes) {
+
+			VentanaRecetas r = new VentanaRecetas();
+			r.setVisible(true);
+			
+		}
+
 	}
-	
-}
 
 
 }
