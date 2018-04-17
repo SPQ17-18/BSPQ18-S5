3ee8de5765e44c7bc5a60889b3b1da7d54ce44f5
arosa001 <arosa001@opendeusto.es>
2018-04-17 14:20:28 +0200

    Ventanas de navegacion

---
+++ NeverEmptyClient/src/main/java/GUI/VentanaFavoritos.java
@@ -0,0 +1,45 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class VentanaFavoritos extends JFrame{
+
+	public VentanaFavoritos() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		this.setSize(400, 375);
+		this.setTitle("Bienvenido a NeverEmpty!"); 
+		this.setResizable(false);
+		setLayout(null);
+		
+		this.setLocationRelativeTo(null);
+		inicializarComponentes();   // inicializamos los atributos o componentes                   
+}
+	
+	
+	private void inicializarComponentes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ NeverEmptyClient/src/main/java/GUI/VentanaRecetas.java
@@ -0,0 +1,33 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class VentanaRecetas extends JFrame{
+
+	
+	
+	public VentanaRecetas() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		this.setSize(400, 375);
+		this.setTitle("Bienvenido a NeverEmpty!"); 
+		this.setResizable(false);
+		setLayout(null);
+		
+		this.setLocationRelativeTo(null);
+		inicializarComponentes();   // inicializamos los atributos o componentes                   
+}
+	
+	
+	private void inicializarComponentes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
