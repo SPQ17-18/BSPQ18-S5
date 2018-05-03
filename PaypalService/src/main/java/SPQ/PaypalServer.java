db7cb461e70162760ed7b5675f82542a4ec63f54
EnaraEtxaniz <enara96etxaniz@gmail.com>
2018-05-03 20:50:03 +0200

    [ADD]Terminado PaypalService

---
+++ PaypalService/src/main/java/SPQ/PaypalServer.java
@@ -0,0 +1,34 @@
+package SPQ;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class PaypalServer {
+
+	
+private static int numClients = 0;
+	
+	public static void main(String args[]) {
+		if (args.length < 1) {
+			System.err.println(" # Usage: GoogleServer [PORT]");
+			System.exit(1);
+		}
+		
+		//args[1] = Server socket port
+		int serverPort = Integer.parseInt(args[0]);
+		
+		try {
+			ServerSocket tcpServerSocket = new ServerSocket(serverPort);
+			System.out.println(" - GoogleServer: Waiting for connections '" + tcpServerSocket.getInetAddress().getHostAddress() + ":" + tcpServerSocket.getLocalPort() + "' ...");
+			
+			while (true) {
+				//Ejecuta el googleservice
+				new PaypalService(tcpServerSocket.accept());
+				System.out.println(" - GoogleServer: New client connection accepted. Client number: " + ++numClients);
+			}
+		} catch (IOException e) {
+			System.err.println("# GoogleServer: IO error:" + e.getMessage());
+		}
+	}
+	
+}
---
+++ PaypalService/src/main/java/SPQ/data/User.java
@@ -0,0 +1,35 @@
+package SPQ.data;
+
+import java.io.Serializable;
+
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable(detachable = "true")
+@Inheritance(strategy = InheritanceStrategy.NEW_TABLE)
+public class User implements Serializable{
+	private String email;
+	private String password;
+	
+	public User(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
