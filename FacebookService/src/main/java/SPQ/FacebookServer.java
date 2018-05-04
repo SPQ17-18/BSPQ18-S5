eebe2d0de4d02782ea49422b9b04c29f6bcc942f
EnaraEtxaniz <enara96etxaniz@gmail.com>
2018-05-04 03:06:15 +0200

    [ADD] AÃadido FacebookServer

--- FacebookService/src/main/java/SPQ/FacebookServer.java
+++ FacebookService/src/main/java/SPQ/FacebookServer.java
@@ -3,13 +3,13 @@ package SPQ;
 import java.io.IOException;
 import java.net.ServerSocket;
 
-public class GoogleServer {
+public class FacebookServer {
 	
 private static int numClients = 0;
 	
 	public static void main(String args[]) {
 		if (args.length < 1) {
-			System.err.println(" # Usage: GoogleServer [PORT]");
+			System.err.println(" # Usage: FacebookServer [PORT]");
 			System.exit(1);
 		}
 		
@@ -18,15 +18,15 @@ private static int numClients = 0;
 		
 		try {
 			ServerSocket tcpServerSocket = new ServerSocket(serverPort);
-			System.out.println(" - GoogleServer: Waiting for connections '" + tcpServerSocket.getInetAddress().getHostAddress() + ":" + tcpServerSocket.getLocalPort() + "' ...");
+			System.out.println(" - FacebookServer: Waiting for connections '" + tcpServerSocket.getInetAddress().getHostAddress() + ":" + tcpServerSocket.getLocalPort() + "' ...");
 			
 			while (true) {
 				//Ejecuta el googleservice
-				new GoogleService(tcpServerSocket.accept());
-				System.out.println(" - GoogleServer: New client connection accepted. Client number: " + ++numClients);
+				new FacebookService(tcpServerSocket.accept());
+				System.out.println(" - FacebookServer: New client connection accepted. Client number: " + ++numClients);
 			}
 		} catch (IOException e) {
-			System.err.println("# GoogleServer: IO error:" + e.getMessage());
+			System.err.println("# FacebookServer: IO error:" + e.getMessage());
 		}
 	}
 }
